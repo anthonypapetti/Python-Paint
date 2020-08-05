5c472463161d6bd63da18815c00224c82118c377
anthonypapetti <apapetti@outlook.com>
2020-08-05 12:34:07 -0400

    Added painting grid

---
+++ elements.py
@@ -0,0 +1,49 @@
+import pygame
+
+class Cell():
+    def __init__(self, posx, posy):
+        self.image = pygame.Surface((5, 5))
+        self.image.fill((255, 255, 255))
+        self.color = (255, 255, 255)
+        self.rect = self.image.get_rect()
+        self.rect.x = posx
+        self.rect.y = posy
+
+    #draws onto screen
+    def Draw(self, surface):
+        surface.blit(self.image, self.rect)
+    
+    #checks for clicks
+    def Click(self, mouse):
+        if mouse[0] > self.rect.topleft[0] and mouse[0] < self.rect.topright[0]:
+            if mouse[1] > self.rect.topleft[1] and mouse[1] < self.rect.bottomleft[1]:
+                return True
+
+class ColorPallate():
+    def __init__(self, posx, posy):
+        self.image = pygame.Surface((50, 50))
+        self.rect = self.image.get_rect()
+        self.rect.x = posx
+        self.rect.y = posy
+    
+    #draws onto screen
+    def Draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+    #checks for clicks    
+    def Click(self, mouse):
+        if mouse[0] > self.rect.topleft[0] and mouse[0] < self.rect.topright[0]:
+            if mouse[1] > self.rect.topleft[1] and mouse[1] < self.rect.bottomleft[1]:
+                return True
+
+class Border():
+    def __init__(self, posx, posy):
+        self.image = pygame.Surface((1000, 5))
+        self.image.fill((201,201,201))
+        self.rect = self.image.get_rect()
+        self.rect.x = posx
+        self.rect.y = posy
+    
+    #draws onto screen
+    def Draw(self, surface):
+        surface.blit(self.image, self.rect)
--- run.py
+++ run.py
@@ -1,35 +1,64 @@
 import pygame
 from pygame.locals import *
 import sys
+from elements import *
 
 pygame.init()
 
 #colors
 colors = {
-    "white": [255, 255, 255],
-    "black": [0,0,0],
-    "red": [255, 0, 0],
-    "blue": [0, 255, 0],
-    "green": [0, 0, 255],
-    "pink": [255, 153, 238],
-    "purple": [187, 51, 255],
-    "teal": [128, 255, 255],
-    "yellow": [255, 255, 0]
+    "white": (255, 255, 255),
+    "black": (0,0,0),
+    "red": (255, 0, 0),
+    "blue": (0, 255, 0),
+    "green": (0, 0, 255),
+    "pink": (255, 153, 238),
+    "purple": (187, 51, 255),
+    "teal": (128, 255, 255),
+    "yellow": (255, 255, 0)
     }
 
-#screen size
+#initalize screen
 size = (1000, 700)
-
+cellsize = (int(size[0]/5), int(550/5))
+print(cellsize)
 screen = pygame.display.set_mode(size)
-
 screen.fill(colors["white"])
+
+#init UI elements
+border = Border(0, 550)
+
+#init cell grid
+grid = []
+
+xptr = 0
+yptr = 0
+for i in range(cellsize[1]):
+    row = []
+    for j in range(cellsize[0]):
+        row.append(Cell(xptr, yptr))
+        xptr += 5
+    yptr += 5
+    xptr = 0
+    grid.append(row)
+
 
 run = True
 while run:
+    mouse = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
+    #draw UI elements
+    border.Draw(screen)
+
+    #draw cell grid
+    for row in grid:
+        for cell in row:
+            cell.Draw(screen)
+    
+    #update display
     pygame.display.update()
 
 pygame.quit()
